e4627c5ba46f1c8e6dcc48cbddeeab85cd261477
hsby <hansenboyi@qq.com>
2021-01-12 11:12:10 +0800

    提交期末作业

---
+++ 期末作业MusicPlayer-master/app/src/main/java/com/goodjob/musicplayer/util/AudioToAudioItem.java
@@ -0,0 +1,8 @@
+package com.goodjob.musicplayer.util;
+
+import com.goodjob.musicplayer.entity.Audio;
+import com.goodjob.musicplayer.entity.AudioItem;
+
+public interface AudioToAudioItem {
+    AudioItem apply(Audio audio);
+}
